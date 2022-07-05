dc46fdd43ed06c7498a27bf586de75b9ec435e70
Igor Yevdokimov <evdokimov048@gmail.com>
2022-07-05 19:15:03 +0300

    ready

---
+++ src/models/ShopItemModel.js
@@ -0,0 +1,10 @@
+class ShopItemModel {
+  constructor (brand, title, description, descriptionFull,price, currency) {
+    this.brand = brand;
+    this.title = title;
+    this.description = description;
+    this.descriptionFull = descriptionFull;
+    this.currency = currency;
+    this.price = price
+  }
+}
